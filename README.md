099d7bb4c871f1564a1d80941d9e651d4d8b8f32
Sasha Priadchenko <56647239+eternalvision@users.noreply.github.com>
2023-05-21 23:27:40 +0200

    added files

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# ITStep-FrontEnd-JS-Materials
-
+Введение
---
+++ index.js
@@ -0,0 +1,15 @@
+// console.log(3 == "3");
+
+// let login = NaN;
+// let pass = "qwe123";
+
+// if (!login && pass !== "jgowjgp") {
+//     console.log("error");
+// }
+
+let mass = { name: "Alexandr" };
+console.log(mass.name);
+
+let age = 1312;
+
+console.log(typeof age);
